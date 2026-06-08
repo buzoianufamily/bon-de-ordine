8b594cbd73be8c5cf88166549604cf94cf4409f4
buzoianufamily <buzoianufamily@gmail.com>
2026-06-08 09:38:52 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -10,9 +10,9 @@ return [
     // ---- Baza de date (din cPanel > MySQL Databases) ----
     'db' => [
         'host'    => 'localhost',
-        'name'    => 'NUMEBD_queue',      // ex: cont_queue
-        'user'    => 'NUMEBD_user',       // ex: cont_user
-        'pass'    => 'PAROLA_BD',
+        'name'    => 'r140100buzo_bon',      // ex: cont_queue
+        'user'    => 'r140100buzo_bon',       // ex: cont_user
+        'pass'    => 'bondeordine',
         'charset' => 'utf8mb4',
     ],
 
@@ -27,8 +27,8 @@ return [
 
     // Token folosit O SINGURA DATA de install.php pentru a crea primul admin.
     // Schimba-l cu ceva aleatoriu inainte sa rulezi install.php.
-    'setup_token' => 'SCHIMBA_ACEST_TOKEN_123',
+    'setup_token' => 'test123',
 
     // Cheie secreta pentru sesiuni/CSRF (schimb-o cu un string aleatoriu lung)
-    'app_key' => 'SCHIMBA_ACEASTA_CHEIE_cu_un_string_lung_aleatoriu',
+    'app_key' => 'test123',
 ];
